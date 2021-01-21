ef1d82641e142644438506fbc255a4fded875e75
Zephyr <zephyrxvxx7@gmail.com>
2021-01-21 16:05:21 +0800

    Add "GET resource limit by user" method

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -3,9 +3,10 @@ from fastapi import APIRouter
 from .endpoints.authenticaion import router as auth_router
 from .endpoints.user import router as user_router
 from .endpoints.project import router as project_router
+from .endpoints.resource_limit import router as resource_limit_router
 
 router = APIRouter()
 router.include_router(auth_router)
 router.include_router(user_router)
 router.include_router(project_router)
-
+router.include_router(resource_limit_router)
