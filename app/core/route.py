a2e29c0c2998037343cd9907b051fd45567d142f
Zephyr <zephyrxvxx7@gmail.com>
2021-06-20 05:34:32 +0800

    add the change password page

--- app/core/route.py
+++ app/core/route.py
@@ -8,8 +8,24 @@ from app.models.user import User
 from .route_user import generate_user_route
 from .route_admin import generate_admin_route
 
+common_route = RouteItem(**{
+    'path': '/change-password',
+    'name': 'ChangePassword',
+    'component': '/sys/changePassword/index',
+    'meta': {
+        "title": 'sys.changePassword.title',
+        "icon": 'carbon:password',
+    },
+})
+
 def getMenuListByUser(user: User, core_v1_api: CoreV1Api) -> List[RouteItem]:
+    menu = []
+    
     if(check_permission(user.roles, RoleEnum.USER)):
-        return generate_user_route(user=user, core_v1_api=core_v1_api)
+        menu = generate_user_route(user=user, core_v1_api=core_v1_api)
     elif(check_permission(user.roles, RoleEnum.SUPER)):
-        return generate_admin_route(user=user, core_v1_api=core_v1_api)
+        menu = generate_admin_route(user=user, core_v1_api=core_v1_api)
+    
+    menu.append(common_route)
+    
+    return menu
